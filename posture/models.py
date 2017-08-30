1203df510e4e65182ec55cc59cafae554a4b580f
rabbani <nawras.rabbani@mail.mcgill.ca>
2017-08-30 11:11:37 -0400

    added the white line indicator in SVG

--- posture/models.py
+++ posture/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 import uuid
 
-# Create your models here.
-
 class User(models.Model):
     user_id = models.CharField(max_length=200, null=True)
-
-    user_baseline_photo = models.ImageField(null=True)
-
     user_baseline_eyes_height = models.IntegerField(null=True)
     user_latest_eyes_height = models.IntegerField(null=True)
--- posture/views.py
+++ posture/views.py
@@ -15,28 +15,39 @@ def frontPage(request):
 def captureBaseline(request):
     return render(request, 'posture/captureBaseline.html')
 
+# tracker is the page that tracks the user's posture and gives and alert if they are slouching
 def tracker(request):
+
+    # make sure that the user's picture has been recieved from the captureBaseline page
     try:
         user_photo_url = request.POST['user_photo_url']
     except:
         context = { "error_title" : "No webcam feed recieved", "error_message" : "Make sure that your webcam is enabled"}
         return render(request, 'posture/errorPage.html', context)
 
+    # open a session for the user that will store its posture information
     request.session['user_id'] = str(uuid.uuid1())
     user_id = request.session.get('user_id')
 
+    # decode the base64 string into an image
     user_baseline_photo = base64decode.base64ToImg(user_photo_url)
 
+    # try to process that image by opencv for face and eye detection
     try:
         user_baseline_eyes_height = opencv.get_eyes_height(user_baseline_photo)
+        # add a little tolerance for correct posture
+        user_baseline_eyes_height += 10
     except:
         context = { "error_title" : "Your eyes could not be detected", "error_message" : "Please try to adjust the light in the room to make your face more visible"}
         return render(request, 'posture/errorPage.html', context)
 
-    User.objects.create(user_id = user_id, user_baseline_photo = user_baseline_photo,
-                        user_baseline_eyes_height = user_baseline_eyes_height)
+    # store the user's information into the database
+    User.objects.create(user_id = user_id, user_baseline_eyes_height = user_baseline_eyes_height)
 
-    return render(request, 'posture/tracker.html')
+    # create a context that contains the user_baseline_eyes_height, this will be used to draw the indicator line on the video
+    context = { "user_baseline_eyes_height" : user_baseline_eyes_height }
+
+    return render(request, 'posture/tracker.html', context)
 
 def assertPosture(request):
     try:
